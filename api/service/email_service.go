e350cc47210148d8e62a52cd52ab6c2fab7184ab
Saksham Yadav <sakshamyadav@Sakshams-MacBook-Air-2.local>
2025-01-18 23:13:00 -0800

    initial commit

--- api/service/email_service.go
+++ api/service/email_service.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
-	"github.com/Sakshamyadav19/emailtracker/config"
+	"github.com/Sakshamyadav19/emailtracker/api/config"
 )
 
 type EmailData struct {
